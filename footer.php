a424ff3c90eb04a869eba10b706d1eadcde9414d
Josue Mora <hmora@where2getit.com>
2015-05-12 10:21:46 -0700

    Added script to enable livereload on localhost

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
     </div>
 </footer>
 
+<!-- Enable live reload -->
+<script src="http://localhost:35729/livereload.js"></script>
 <!-- Load javascript at bottom for better performance -->
 <script type="text/javascript" src="static/js/main.js"></script>
 <script type="text/javascript" src="static/js/less.js"></script>
